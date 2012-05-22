4342e103b9b355b16bdb4ef3b1c9590c44b02332
mkillianey <mkillianey@guidewire.com>
2012-05-22 12:39:33 -0700

    Update Solver.java using the github edit window.

--- src/codejam/Solver.java
+++ src/codejam/Solver.java
@@ -3,9 +3,19 @@ package codejam;
 import java.io.BufferedReader;
 
 /**
- * Single abstract method.
+ * Solver of Google codejam problems.
+ *
+ * Note: This interface should contain only a single method so that
+ * it can be invoked in Gosu by passing a block with a signature that
+ * matches the {@link #solve} method.
  */
 public interface Solver {
 
+  /**
+   * Reads exactly one Google codejam case from the given reader.
+   *
+   * @return the answer to the case (the part that appears after "Case #n: ")
+   */
   String solve(BufferedReader reader);
 }
+
